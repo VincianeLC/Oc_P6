3a2761b8357e76e7fbf35804e72a144c029b311c
DigitalBlooming <102424308+VincianeLC@users.noreply.github.com>
2022-10-05 14:41:45 +0200

    Update sauces.js

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -44,7 +44,7 @@ exports.modifySauce = (req, res, next) => {
 exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then(sauce => {
-      if (sauce.userId != req.auth.userId) {
+      if (sauce._userId != req.auth.userId) {
         res.status(401).json({ message: 'Not authorized' });
       } else {
         const filename = sauce.imageUrl.split('/images/')[1];
